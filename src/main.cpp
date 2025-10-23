4f5049ea4ce1d3433ee65a731391b09eaceb8792
Nahom Garefo <harobina28@gmail.com>
2025-10-23 17:38:57 +0300

    structure and read tree object

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,15 @@
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <type_traits>
 #include <vector>
 #include <zlib.h>
 
-void readTreeObject(const std::string& treeSha);
 struct GitObject{
     std::string dirName;
     std::string fileName;
-    std::string content;
-    std::string name;
+    std::string_view content;
+    std::string type;
     size_t size;
 
     GitObject(std::string name)
@@ -28,9 +28,72 @@ struct GitObject{
     }
 };
 
+struct TreeEntries{
+    std::string_view mode;
+    std::string_view name;
+    std::string_view hash;
+    std::string_view type;
+};
+class Tree{
+    public:
+        std::vector<TreeEntries> entries;
+
+    public:
+        void parseTree(const std::string_view& content);
+};
+
+void readTreeObject(const std::string& treeSha);
+void readBlobObject(std::string& output, const std::string& fileName);
+void zlibDecompression(std::string_view& decompressedView, GitObject& object);
+void createBlobObject(std::string& hash, std::string inputFile);
+void byteToHexHash(std::string& hexHash, const unsigned char* byteHash, size_t len);
 
 
-void readGitObject(std::string& output, const std::string& fileName)
+
+void Tree::parseTree(const std::string_view& content)
+{
+    size_t l = content.size();
+    size_t pos=0;
+    while (pos < l)
+    {
+        // mode 
+        size_t spacePos = content.find(' ',pos);
+        if(spacePos == std::string::npos) break;
+        std::string_view mode = content.substr(pos,spacePos-pos);
+        pos= spacePos + 1;
+
+        // name  up to null byte
+        size_t nullPos = content.find('\0', pos);
+        if(spacePos == std::string::npos) break;
+        std::string_view name = content.substr(pos,nullPos-pos);
+        pos= nullPos + 1;
+
+        // 20 bit SHA-1 binary hash
+        std::string_view byteHash = content.substr(pos,20);
+        pos += 20;
+        std::string hexHash;
+        std::vector<unsigned char> data_vect(byteHash.begin(), byteHash.end());
+        byteToHexHash(hexHash, data_vect.data(),data_vect.size());
+
+        TreeEntries entry{mode,name,hexHash};
+        if (mode == "40000")
+        {
+            entry.type = "tree";
+        }
+        else if (mode == "100644")
+        {
+            entry.type = "blob";
+        }
+        else
+        {
+            entry.type = "blob";
+        }
+        entries.push_back(entry);
+
+    }
+}
+
+void readBlobObject(std::string& output, const std::string& fileName)
 {
     GitObject blobObject(fileName);
 
@@ -38,57 +101,9 @@ void readGitObject(std::string& output, const std::string& fileName)
     {
         throw std::runtime_error("git object name must be at least 2 characters long!");
     }
+    std::string_view decompressedView;
+    zlibDecompression(decompressedView, blobObject);
 
-    std::string path("./.git/objects/"+ blobObject.dirName + "/" + blobObject.fileName);
-    std::ifstream file(path, std::ios::binary);
-    if (!file)
-    {
-        throw std::runtime_error("failed to open file " + path);
-    }
-    // save file in memory for decompression 
-    std::vector<unsigned char> compressed(
-        (std::istreambuf_iterator<char>(file)),
-        std::istreambuf_iterator<char>()
-    );
-
-    // decompress the file using zlib
-    z_stream strm{};
-    strm.next_in = compressed.data();
-    strm.avail_in = compressed.size();
-
-    if(inflateInit(&strm) != Z_OK)
-    {
-        throw std::runtime_error("inflateInit failed");
-    }
-
-    const size_t CHUNK_SIZE = 4096;
-    std::vector<unsigned char> buffer(CHUNK_SIZE);
-    std::vector<unsigned char> decompressed;
-
-    int ret;
-    do{
-        strm.next_out = buffer.data();
-        strm.avail_out = buffer.size();
-
-        ret = inflate(&strm, Z_NO_FLUSH);
-        if (ret != Z_OK && ret != Z_STREAM_END && ret != Z_BUF_ERROR)
-        {
-            inflateEnd(&strm);
-            throw std::runtime_error("Zlib inflate failed!");
-        }
-        decompressed.insert(decompressed.end(), 
-                            buffer.begin(),
-                            buffer.begin() + (buffer.size() - strm.avail_out)
-                            );
-    
-    } while (ret != Z_STREAM_END);
-
-    inflateEnd(&strm);
-
-    /// split the content into headers and contents
-    std::string_view decompressedView(
-                                reinterpret_cast<const char*>(decompressed.data())
-                                , decompressed.size());
     size_t nullPos = decompressedView.find('\0');
     if (nullPos == std::string::npos)
     {
@@ -98,78 +113,9 @@ void readGitObject(std::string& output, const std::string& fileName)
 
     std::string_view header = decompressedView.substr(0,nullPos);
     size_t spacePos = header.find(' ');
-    blobObject.name = header.substr(0,spacePos);
+    blobObject.type = header.substr(0,spacePos);
     blobObject.size = std::stol(std::string(header.substr(spacePos+1)));
 
-}
-
-void createBlobObject(std::string& hash, std::string inputFile)
-{
-    // open file 
-    std::ifstream file(inputFile, std::ios::binary);
-    if(!file)
-    {
-        throw std::runtime_error("failed to open file " + inputFile);
-    }
-    std::ostringstream oss;
-    oss << file.rdbuf();
-    std::string temp(oss.str());
-
-    // produce SHA-1 hash from the content + size + blob
-    std::string uncompressed("blob " + std::to_string(temp.size()) + '\0' + temp);
-    unsigned char byteHash[SHA_DIGEST_LENGTH];
-    SHA1(reinterpret_cast<const unsigned char*>(uncompressed.c_str()),  
-        uncompressed.size(),
-        byteHash);
-    // convert the 20 byte hash to 40 character of hexadecimal 
-    std::ostringstream charHash;
-    for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
-    {
-         charHash << std::hex << std::setw(2) <<std::setfill('0')<<(int) byteHash[i];
-    }
-    std::string hashStr = charHash.str();
-    std::string dirNamePart = hashStr.substr(0,2);
-    std::string fileNamePart = hashStr.substr(2);
-
-    // compress the  content + size + blob
-
-    // const size_t CHUNK_SIZE = 4096;
-    // std::vector<unsigned char> buffer(CHUNK_SIZE);
-    uLong uncompressedSize = uncompressed.size();
-    uLong compressedSize = compressBound(uncompressedSize);
-    std::vector<Bytef> compressed(compressedSize);
-
-    int ret = compress(compressed.data(), 
-                    &compressedSize, 
-                     reinterpret_cast<const Bytef*>(uncompressed.data()), 
-                  uncompressedSize);
-    
-    if(ret != Z_OK)
-    {
-        std::cerr << "Unable to compress file using zlib";
-        return;
-    }
-    compressed.resize(compressedSize);
-
-    // create the SHA-1 file inside the sha1[:2] folder 
-    std::filesystem::path folderPath(".git/objects/" + dirNamePart);
-    if(!std::filesystem::create_directories(folderPath))
-    {
-        std::cerr << "failed to create directory " << folderPath.string();
-        return;
-    }
-    std::string fileName(folderPath.string() + "/" + fileNamePart);
-    std::ofstream hashFile(fileName, std::ios::binary);
-    if (!hashFile.is_open())
-    {
-        std::cerr << "failed to create or open file " << fileName; 
-        return;
-    }
-    // write the compressed file to it 
-    hashFile.write(reinterpret_cast<const char*>(compressed.data()), compressed.size());
-    hashFile.close();
-    // print the file and folder name for verification for the tester
-    hash = hashStr;
 }
 
 int main(int argc, char *argv[])
@@ -221,7 +167,7 @@ int main(int argc, char *argv[])
         std::string output;
         try 
         {
-            readGitObject(output, argv[3]);
+            readBlobObject(output, argv[3]);
             std::cout << output;
 
         } catch (const std::exception& e)
@@ -250,9 +196,11 @@ int main(int argc, char *argv[])
 
     else if(command == "ls-tree")
     {
-        if (argc !=4 || argv[2] !="--name-only")
+        if (argc !=4 || std::string(argv[2]) !="--name-only")
         {
             std::cerr << "wrong paramter usage --name-only <tree_sha>";
+            std::cerr << argc;
+            std::cerr << "argv[2]" << argv[2];
             return EXIT_FAILURE;
         }
         readTreeObject(argv[3]);
@@ -269,5 +217,149 @@ int main(int argc, char *argv[])
 
 void readTreeObject(const std::string& treeSha)
 {
+    GitObject treeObject(treeSha);
+    if (treeObject.fileName.length() < 2)
+    {
+        throw std::runtime_error("git object name must be at least 2 characters long!");
+    }
+    std::string_view decompressedView;
+    zlibDecompression(decompressedView, treeObject);
+    Tree tree;
+    tree.parseTree(decompressedView);
+
+    // console log 
+
+    for (TreeEntries& entries:tree.entries)
+    {
+        std::cout << entries.name << "\n";
+    }
+}
+
+void zlibDecompression(std::string_view& decompressedView, GitObject& object)
+{
+    std::string path("./.git/objects/"+ object.dirName + "/" + object.fileName);
+    std::ifstream file(path, std::ios::binary);
+    if (!file)
+    {
+        throw std::runtime_error("failed to open file " + path);
+    }
+    // save file in memory for decompression 
+    std::vector<unsigned char> compressed(
+        (std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>()
+    );
+
+    // decompress the file using zlib
+    z_stream strm{};
+    strm.next_in = compressed.data();
+    strm.avail_in = compressed.size();
+
+    if(inflateInit(&strm) != Z_OK)
+    {
+        throw std::runtime_error("inflateInit failed");
+    }
+
+    const size_t CHUNK_SIZE = 4096;
+    std::vector<unsigned char> buffer(CHUNK_SIZE);
+    std::vector<unsigned char> decompressed;
+
+    int ret;
+    do{
+        strm.next_out = buffer.data();
+        strm.avail_out = buffer.size();
+
+        ret = inflate(&strm, Z_NO_FLUSH);
+        if (ret != Z_OK && ret != Z_STREAM_END && ret != Z_BUF_ERROR)
+        {
+            inflateEnd(&strm);
+            throw std::runtime_error("Zlib inflate failed!");
+        }
+        decompressed.insert(decompressed.end(), 
+                            buffer.begin(),
+                            buffer.begin() + (buffer.size() - strm.avail_out)
+                            );
     
+    } while (ret != Z_STREAM_END);
+
+    inflateEnd(&strm);
+
+    /// split the content into headers and contents
+    decompressedView = std::string_view(
+                                reinterpret_cast<const char*>(decompressed.data())
+                                , decompressed.size());
+}
+
+void createBlobObject(std::string& hash, std::string inputFile)
+{
+    // open file 
+    std::ifstream file(inputFile, std::ios::binary);
+    if(!file)
+    {
+        throw std::runtime_error("failed to open file " + inputFile);
+    }
+    std::ostringstream oss;
+    oss << file.rdbuf();
+    std::string temp(oss.str());
+
+    // produce SHA-1 hash from the content + size + blob
+    std::string uncompressed("blob " + std::to_string(temp.size()) + '\0' + temp);
+    unsigned char byteHash[SHA_DIGEST_LENGTH];
+    SHA1(reinterpret_cast<const unsigned char*>(uncompressed.c_str()),  
+        uncompressed.size(),
+        byteHash);
+    // convert the 20 byte hash to 40 character of hexadecimal 
+
+    std::string hashStr;
+    byteToHexHash(hashStr, byteHash, SHA_DIGEST_LENGTH);
+    std::string dirNamePart = hashStr.substr(0,2);
+    std::string fileNamePart = hashStr.substr(2);
+
+    // compress the  content + size + blob
+
+    // const size_t CHUNK_SIZE = 4096;
+    // std::vector<unsigned char> buffer(CHUNK_SIZE);
+    uLong uncompressedSize = uncompressed.size();
+    uLong compressedSize = compressBound(uncompressedSize);
+    std::vector<Bytef> compressed(compressedSize);
+
+    int ret = compress(compressed.data(), 
+                    &compressedSize, 
+                     reinterpret_cast<const Bytef*>(uncompressed.data()), 
+                  uncompressedSize);
+    
+    if(ret != Z_OK)
+    {
+        std::cerr << "Unable to compress file using zlib";
+        return;
+    }
+    compressed.resize(compressedSize);
+
+    // create the SHA-1 file inside the sha1[:2] folder 
+    std::filesystem::path folderPath(".git/objects/" + dirNamePart);
+    if(!std::filesystem::create_directories(folderPath))
+    {
+        std::cerr << "failed to create directory " << folderPath.string();
+        return;
+    }
+    std::string fileName(folderPath.string() + "/" + fileNamePart);
+    std::ofstream hashFile(fileName, std::ios::binary);
+    if (!hashFile.is_open())
+    {
+        std::cerr << "failed to create or open file " << fileName; 
+        return;
+    }
+    // write the compressed file to it 
+    hashFile.write(reinterpret_cast<const char*>(compressed.data()), compressed.size());
+    hashFile.close();
+    // print the file and folder name for verification for the tester
+    hash = hashStr;
+}
+
+void byteToHexHash(std::string& hexHash, const unsigned char* byteHash, size_t len){
+    std::ostringstream charHash;
+    for (int i = 0; i < len; i++)
+    {
+         charHash << std::hex << std::setw(2) <<std::setfill('0')<<(int) byteHash[i];
+    }
+    hexHash = charHash.str();
 }
